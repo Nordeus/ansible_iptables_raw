470089dc22bcc6cfba0876500101eed48449a770
jbonnier <noreply@julienbonnier.com>
2023-11-03 14:32:28 -0400

    fix: Adding fallback empty array return

--- iptables_raw.py
+++ iptables_raw.py
@@ -377,6 +377,7 @@ class Iptables:
                         return table_names.splitlines()
                     else:
                         return self.TABLES
+        return []
 
     # If /etc/debian_version exist, this means this is a debian based OS (Ubuntu, Mint, etc...)
     def _is_debian(self):
